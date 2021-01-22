4a9ef00bff0aad3c125943b507ac1bd81443e2fe
thewingson <almatrakh97@gmail.com>
2021-01-22 21:27:10 +0600

    new messages

--- src/CommonMessages.java
+++ src/CommonMessages.java
@@ -1,6 +1,13 @@
 public class CommonMessages {
 
+    // Exception messages
     public static final String NUMBER_OUT_OF_RANGE = "Your entered number is out of range.";
     public static final String ILLEGAL_TYPE_OF_INPUT = "Illegal type of input. It can be only NUMBER.";
+
+    // Static values
+    public static final String PATH_TO_THE_TEXTFILE = "C:\\Users\\Mi Notebook\\Desktop\\PracticalTask\\src\\textfilereport.txt";
+    public static final String UNABLE_TO_OPEN_FILE = "Unable to open file";
+    public static final String ERROR_AT_WRITING = "Something went wrong while writing";
+    public static final String FILE_IS_UNACCESSIBLE = "File is unaccessible";
 
 }
