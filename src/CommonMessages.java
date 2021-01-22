980a346f7e1c2f6ea5030f33ec7d977af5b1b275
thewingson <almatrakh97@gmail.com>
2021-01-22 17:14:21 +0600

    added some docs

--- src/CommonMessages.java
+++ src/CommonMessages.java
@@ -1,5 +1,6 @@
 public class CommonMessages {
 
     public static final String NUMBER_OUT_OF_RANGE = "Your entered number is out of range.";
+    public static final String ILLEGAL_TYPE_OF_INPUT = "Illegal type of input. It can be only NUMBER.";
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,31 +11,48 @@ public class Main {
         initFields();
 
         System.out.println("Please, enter desired number between 1 and 100:");
-        int desiredNum = in.nextInt(); // TODO: add exception for type
-        if(desiredNum < 1 || desiredNum > 100){
-            throw new IllegalStateException(CommonMessages.NUMBER_OUT_OF_RANGE);
+        int desiredNum;
+        try {
+            desiredNum = in.nextInt();
+            if (desiredNum < 1 || desiredNum > 100) {
+                throw new IllegalStateException(CommonMessages.NUMBER_OUT_OF_RANGE);
+            }
+            printNumbers(desiredNum);
+        } catch (InputMismatchException e){
+            throw new IllegalArgumentException(CommonMessages.ILLEGAL_TYPE_OF_INPUT);
         }
-
-        printNumbers(desiredNum);
-
     }
 
-    private static void initFields(){
+    /**
+     * Simple method for initialization of array values.
+     * */
+    private static void initFields() {
         squareArray = new int[100];
-        for(int i = 1; i <= squareArray.length; i++){
-            squareArray[i-1] = i*i;
+        for (int i = 1; i <= squareArray.length; i++) {
+            squareArray[i - 1] = i * i;
         }
     }
 
-    private static void printNumbers(Integer rangeEnd){
-        for(int i = 0; i < rangeEnd; i++){
-            System.out.printf("(%d)^2 = %d%n", i+1, squareArray[i]);
+    /**
+     * Method for printing squared values.
+     *
+     * @param rangeEnd - last index to read to.
+     * */
+    private static void printNumbers(Integer rangeEnd) {
+        for (int i = 0; i < rangeEnd; i++) {
+            System.out.printf("(%d)^2 = %d%n", i + 1, squareArray[i]);
         }
     }
 
-    private static void printNumbers(Integer rangeStart, Integer rangeEnd){
-        for(int i = rangeStart-1; i < rangeEnd; i++){
-            System.out.printf("(%d)^2 = %d%n", i+1, squareArray[i]);
+    /**
+     * @see Main#printNumbers(Integer rangeEnd) overload
+     *
+     * @param rangeStart - start index to read from.
+     * @param rangeEnd - last index to read to.
+     * */
+    private static void printNumbers(Integer rangeStart, Integer rangeEnd) {
+        for (int i = rangeStart - 1; i < rangeEnd; i++) {
+            System.out.printf("(%d)^2 = %d%n", i + 1, squareArray[i]);
         }
     }
 }
